4aa66c4231a5f1e141490a54a8627a69132f717d
Baukadac <baukadac@gmail.com>
2026-02-12 22:16:50 +0000

    hw 3

---
+++ Terraform/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "7.18.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = "my-project-id"
+  region      = "us-central1"
+}
+
+
